352f0c290558fcf35a375e6d6c9cb29ef083a4d2
Joshua <mramos29@ucol.mx>
2022-06-30 19:45:01 -0500

    Estadisticas

--- ejercicio2/src/main.jsx
+++ ejercicio2/src/main.jsx
@@ -7,21 +7,26 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const[average, setAverage] = useState(0);
+  const[total, setTotal] = useState(0);
 
-
-  const plusGood = () => setGood(good+1);
-  const plusBad = () => setBad(bad+1);
-  const plusNeutral = () => setNeutral(neutral+1);
+  const plusGood = () => {setGood(good+1); setAverage(average+1); setTotal(total+1);};
+  const plusBad = () => {setBad(bad+1); setAverage(average-1); setTotal(total+1);};
+  const plusNeutral = () => {setNeutral(neutral+1); setTotal(total+1);};
   return(
     <div>
     <h1>Give feedback</h1>
     <button onClick={plusGood}>Good</button>
-    <button onClick={plusBad}>Bad</button>
     <button onClick={plusNeutral}>Neutral</button>
+    <button onClick={plusBad}>Bad</button>
     <h1>Stadistics</h1>
     <p>Good: {good}</p>
-    <p>Bad: {bad}</p>
     <p>Neutral: {neutral}</p>
+    <p>Bad: {bad}</p>
+    <br/>
+    <p>Total: {total}</p>
+    <p>Average: {average/total}</p>
+    <p>Acceptance: {(good/total)*100}%</p>
     </div>
   )
 }
